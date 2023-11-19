dfbaa79c2774d6e7fd773578b91385761e764e15
NyakoFox <nyakowofox@gmail.com>
2023-11-19 00:32:33 -0400

    make table text smaller & unyellowify

--- components/Box.jsx
+++ components/Box.jsx
@@ -1,5 +1,8 @@
 import styles from './Box.module.css';
 
-export default function Box({ children }) {
+export default function Box({ children, className }) {
+    if (className) {
+        return <div className={`${styles.box} ${className}`} >{children}</div>;
+    }
     return <div className={styles.box}>{children}</div>;
 }
