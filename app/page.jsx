1dc89f17be7fec88990b425d031123cbf9b37350
NyakoFox <nyakowofox@gmail.com>
2023-11-25 11:52:59 -0400

    update main page, update main wiki page

--- app/page.jsx
+++ app/page.jsx
@@ -7,25 +7,36 @@ export default function Home() {
 
     const mods = [
         {
+            title: "Kris and Susie's Wacky Dark World Adventures",
+            description: "Explore a bunch of fun and exciting dark worlds with new and old friends!",
+            image: "/screenshots/kris_and_susies_wacky_dark_world_adventures.png",
+            page: "https://gamejolt.com/games/deltarune-kris-and-susies-wacky-dark-world/852417",
+            author: "TrashcatYT",
+            author_link: "https://gamejolt.com/@TrashcatYT"
+        },
+        {
             title: "Godhome",
             description: "Take your place amongst the Gods. 40+ boss fights, challenge modes, and a unique ending. Contains major spoilers for Hollow Knight.",
             image: "/screenshots/godhome.png",
             page: "https://gamebanana.com/mods/376524",
-            author: "Vitellary"
+            author: "Vitellary",
+            author_link: null
         },
         {
-            title: "Deltamon",
-            description: "Kris and Susie get swept into the world of Pokemon and head out on an adventure to become the strongest Pokemon trainer. However, things are not quite as they seem, and they soon find themselves up against an imposing force...",
-            image: "/screenshots/deltamon.png",
-            page: null,
-            author: "SylviBlossom"
+            title: "Deoxynn",
+            description: "Alexa Greene, a victim of something terrible, starts her adventure through the world of Europa. Will she make new friends and keep sane, or will she succumb to her insanity? The choice is yours.",
+            image: "/screenshots/deoxynn.png",
+            page: "https://gamebanana.com/mods/434334",
+            author: "AcousticJamm",
+            author_link: "https://www.guilded.gg/i/240D4G02"
         },
         {
-            title: "The Cyber Servers",
-            description: "While on their way to Queen's Mansion, the Fun Gang have to take a detour, riding a teacup into the sky... Not having to worry about Queen for a while, the Fun Gang ends up meeting new friends, new foes and get in quite a lot of trouble...",
-            image: "/screenshots/cyber-servers.png",
-            page: null,
-            author: "NyakoFox"
+            title: "Deltarune: Missing Light",
+            description: "Traverse through different worlds and realities, and save the multiverse from being consumed by darkness and anomalies!",
+            image: "/screenshots/deltarune_missing_light.png",
+            page: "https://gamejolt.com/games/missinglight/799416",
+            author: "DiamondBor",
+            author_link: "https://www.youtube.com/channel/UCs76CI1gJeWx77sSTCCbICA"
         }
     ]
 
@@ -40,13 +51,13 @@ export default function Home() {
                 </Box>
 
                 <Box>
-                    <h2 className={styles.header}>Can I help?</h2>
-                    <b>Yes!</b> Feel free to look through the <NewTab href="https://github.com/KristalTeam/Kristal">source code</NewTab> of Kristal and contribute if you wish.
+                    <h2 className={styles.header}>How do I use it?</h2>
+                    Check out the <Link href="/wiki/">wiki</Link> for more information on how to use Kristal.
                 </Box>
 
                 <Box>
-                    <h2 className={styles.header}>How do I use it?</h2>
-                    Check out the <Link href="/wiki/">wiki</Link> for more information on how to use Kristal.
+                    <h2 className={styles.header}>Can I help?</h2>
+                    <b>Yes!</b> Feel free to look through the <NewTab href="https://github.com/KristalTeam/Kristal">source code of Kristal</NewTab> and contribute if you wish.
                 </Box>
 
                 <Box>
@@ -58,7 +69,7 @@ export default function Home() {
                             const img = <img className={styles.screenshot} src={mod.image}/>;
                             const info = <div className={styles.screenshot_info}>
                                 <NewTab href={mod.page} className={styles.screenshot_title}>{mod.title}</NewTab>
-                                <span className={styles.screenshot_author}>By {mod.author}</span>
+                                <span className={styles.screenshot_author}>By <NewTab href={mod.author_link}>{mod.author}</NewTab></span>
                                 <p className={styles.screenshot_description}>{mod.description}</p>
                                 </div>;
                             return (index % 2 == 0) ? [img, info] : [info, img];
