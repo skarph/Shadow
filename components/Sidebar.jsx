f9c6e3af0017f197d9d81d977e42422692c46692
NyakoFox <nyakowofox@gmail.com>
2023-11-28 16:57:14 -0400

    shuffle things around on the sidebar

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -13,22 +13,34 @@ export default function Sidebar(props) {
             <li><Link href="/wiki/playing-mods">Installing and Playing Mods</Link></li>
         </ul>
         <br/>
-        <h3><Link href="/wiki/mod-creation">Mod Creation</Link></h3>
+        <h3><Link href="/wiki/mod-creation">General Mod Creation</Link></h3>
         <hr/>
         <p>These pages teach you about mod development.</p>
         <ul>
             <li><Link href="/wiki/basics">Understanding the Basics</Link></li>
             <li><Link href="/wiki/creating-a-mod">Creating a Mod</Link></li>
             <li><Link href="/wiki/using-libraries">Using Libraries</Link></li>
-            <li><Link href="/wiki/designing-a-map">Designing a Map</Link></li>
-            <li><Link href="/wiki/cutscenes">Cutscenes</Link></li>
             <li><Link href="/wiki/creating-an-item">Creating an Item</Link></li>
             <li><Link href="/wiki/creating-a-spell">Creating a Spell</Link></li>
             <li><Link href="/wiki/actors">Actors</Link></li>
             <li><Link href="/wiki/party-members">Party Members</Link></li>
+            <li><Link href="/wiki/ui">The UI System</Link></li>
+        </ul>
+        <br/>
+        <h3><Link href="/wiki/mod-creation">The Overworld</Link></h3>
+        <hr/>
+        <p>Everything to do with the overworld.</p>
+        <ul>
+            <li><Link href="/wiki/designing-a-map">Designing a Map</Link></li>
+            <li><Link href="/wiki/cutscenes">Cutscenes</Link></li>
+        </ul>
+        <br/>
+        <h3><Link href="/wiki/mod-creation">Battles</Link></h3>
+        <hr/>
+        <p>Everything related to creating battles.</p>
+        <ul>
             <li><Link href="/wiki/battlers">Battlers</Link></li>
             <li><Link href="/wiki/encounters">Encounters</Link></li>
-            <li><Link href="/wiki/ui">The UI System</Link></li>
         </ul>
         <br/>
         <h3><Link href="/wiki/api">API Reference</Link></h3>
